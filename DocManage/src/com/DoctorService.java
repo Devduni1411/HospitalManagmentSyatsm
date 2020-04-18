45bc347a24ed511c8655e0931c010bed58960a6f
DELL <DELL@kushan>
2020-04-18 18:25:41 +0530

    2nd

--- DocManage/src/com/DoctorService.java
+++ DocManage/src/com/DoctorService.java
@@ -40,9 +40,10 @@ public class DoctorService {
 	@FormParam("address") String address,
 	@FormParam("email") String email,
 	@FormParam("phoneNo") String phoneNo,
-	@FormParam("specialization") String specialization)
+	@FormParam("specialization") String specialization,
+	@FormParam("hospitalID") String hospitalID)
 	{
-	String output = docObj.insertDoctor(dName, address, email, phoneNo, specialization);
+	String output = docObj.insertDoctor(dName, address, email, phoneNo, specialization, hospitalID);
 	return output;
 	}
 	
